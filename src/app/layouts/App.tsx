86205bf8fa91e3617d2358272b3ed09d61fbf7ca
Agustin Carozo <agustin@agustincarozo.com>
2023-11-14 19:28:46 -0300

    First Ui - NavBar

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -1,8 +1,15 @@
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/dashboard/EventDashboard";
+import NavBar from "./nav/NavBar";
+
 function App() {
   return (
-    <>
-      <h1>Welcome to Revents</h1>
-    </>
+    <div>
+      <NavBar />
+      <Container className="main">
+        <EventDashboard />
+      </Container>
+    </div>
   );
 }
 
---
+++ src/app/layouts/nav/NavBar.tsx
@@ -0,0 +1,32 @@
+import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
+
+export default function () {
+  return (
+    <Menu inverted={true} fixed="top">
+      <Container>
+        <MenuItem header>
+          <img src="/logo.png" alt="logo" />
+          Re-vents
+        </MenuItem>
+        <MenuItem name="Events" />
+        <MenuItem>
+          <Button
+            floated="right"
+            positive={true}
+            inverted={true}
+            content="Create event"
+          ></Button>
+        </MenuItem>
+        <MenuItem position="right">
+          <Button basic inverted content="login" />
+          <Button
+            basic
+            inverted
+            content="Register"
+            style={{ marginLeft: "0.5em" }}
+          />
+        </MenuItem>
+      </Container>
+    </Menu>
+  );
+}
